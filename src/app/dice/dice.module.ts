c6d0f68cdd877c9ba533a6db358836fee2592b22
Jake Smith <j.smith@questintegrity.com>
2018-03-03 17:06:14 -0700

    Move dice to service

--- src/app/dice/dice.module.ts
+++ src/app/dice/dice.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { UiModule } from './../ui/ui.module';
 import { DiceRoutingModule } from './dice-routing.module';
 import { RollComponent } from './roll/roll.component';
+import { DiceService } from './dice.service';
 
 @NgModule({
   imports: [
@@ -11,6 +12,7 @@ import { RollComponent } from './roll/roll.component';
     DiceRoutingModule,
     UiModule
   ],
-  declarations: [RollComponent]
+  declarations: [RollComponent],
+  providers: [DiceService]
 })
 export class DiceModule { }
--- src/app/dice/roll/roll.component.ts
+++ src/app/dice/roll/roll.component.ts
@@ -1,11 +1,6 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import * as CANNON from 'cannon';
-import * as THREE from 'three';
-import { Stats } from 'three-stats';
-import { DiceD6, DiceManager, DiceObject } from 'threejs-dice';
 
-declare const require: (moduleId: string) => any;
-const OrbitControls = require('three-orbit-controls')(THREE);
+import { DiceService } from '../dice.service';
 
 @Component({
   selector: 'app-roll',
@@ -16,156 +11,18 @@ export class RollComponent implements OnInit, OnDestroy {
 
   @ViewChild('canvasWrapper') canvasWrapper: ElementRef;
   results: any;
-  diceAddedToScene = false;
   statsShowing = true;
-  private _world: CANNON.World;
-  private _dice: DiceD6[] = [];
-  private _scene: THREE.Scene;
-  private _renderer: THREE.WebGLRenderer;
-  private _camera: THREE.PerspectiveCamera;
-  private _controls: any;
-  private _stats: any;
-  private _throwRunning: boolean;
-  private readonly VIEW_ANGLE = 45;
-  private readonly NEAR = 0.01;
-  private readonly FAR = 20000;
 
-  constructor(private _self: ElementRef) { }
-
-  private get width() {
-    return window.innerWidth - 48;
-  }
-
-  private get height() {
-    return window.innerHeight - 400;
-  }
-
-  private get aspect() {
-    return this.width / this.height;
-  }
+  constructor(
+    private _self: ElementRef,
+    private _diceService: DiceService
+  ) { }
 
   ngOnInit() {
-    window.addEventListener('resize', () => {
-      this._camera.aspect = this.aspect;
-      this._camera.updateProjectionMatrix();
-      this._renderer.setSize(this.width, this.height);
-    });
-    this._scene = new THREE.Scene();
-
-    // CAMERA
-    this._camera = new THREE.PerspectiveCamera(this.VIEW_ANGLE, this.aspect, this.NEAR, this.FAR);
-    this._scene.add(this._camera);
-    this._camera.position.set(0, 30, 30);
-
-    // RENDERER
-    this._renderer = new THREE.WebGLRenderer({ antialias: true });
-    this._renderer.setSize(this.width, this.height);
-    this._renderer.shadowMap.enabled = true;
-    this._renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-    // CONTAINER
     const container = this.canvasWrapper.nativeElement;
-    container.appendChild(this._renderer.domElement);
-
-    // CONTROLS
-    this._controls = new OrbitControls(this._camera, this._renderer.domElement);
-
-    // STATS
-    this._stats = new Stats();
-    this._stats.domElement.style.position = 'relative';
-    this._stats.domElement.style.top = '-90%';
-    this._stats.domElement.style.left = '24px';
-    this._stats.domElement.style.zIndex = 100;
-    container.appendChild(this._stats.domElement);
-
-    const ambient = new THREE.AmbientLight('#ffffff', 0.3);
-    this._scene.add(ambient);
-
-    const directionalLight = new THREE.DirectionalLight('#ffffff', 0.5);
-    directionalLight.position.x = -1000;
-    directionalLight.position.y = 1000;
-    directionalLight.position.z = 1000;
-    this._scene.add(directionalLight);
-
-    const light = new THREE.SpotLight(0xefdfd5, 1.3);
-    light.position.y = 100;
-    light.castShadow = true;
-    light.shadow.camera.near = 50;
-    light.shadow.camera.far = 110;
-    light.shadow.mapSize.width = 1024;
-    light.shadow.mapSize.height = 1024;
-    this._scene.add(light);
-
-    // FLOOR
-    const floorMaterial = new THREE.MeshPhongMaterial({ color: '#222222', side: THREE.DoubleSide });
-    const floorGeometry = new THREE.PlaneGeometry(30, 30, 10, 10);
-    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.receiveShadow = true;
-    floor.rotation.x = Math.PI / 2;
-    this._scene.add(floor);
-
-    // WALL 1
-    const wallMaterial = new THREE.MeshPhongMaterial({ color: '#222222', side: THREE.DoubleSide });
-    const wallGeometry = new THREE.PlaneGeometry(30, 5, 10, 10);
-    const wall = new THREE.Mesh(wallGeometry, wallMaterial);
-    wall.receiveShadow = true;
-    wall.rotation.y = Math.PI / 2;
-    wall.translateZ(15);
-    wall.translateY(2.5);
-    this._scene.add(wall);
-
-    // WALL 2
-    const wall2 = new THREE.Mesh(wallGeometry, wallMaterial);
-    wall2.receiveShadow = true;
-    wall2.translateZ(15);
-    wall2.translateY(2.5);
-    this._scene.add(wall2);
-
-    // SKYBOX/FOG
-    const skyBoxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
-    const skyBoxMaterial = new THREE.MeshPhongMaterial({ color: 0x9999ff, side: THREE.BackSide });
-    const skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
-    // this._scene.add(skyBox);
-    this._scene.fog = new THREE.FogExp2(0x9999ff, 0.00025);
-
-    ////////////
-    // CUSTOM //
-    ////////////
-    this._world = new CANNON.World();
-    this._world.gravity.set(0, -9.82 * 20, 0);
-    this._world.broadphase = new CANNON.NaiveBroadphase();
-    this._world.solver.iterations = 16;
-
-    DiceManager.setWorld(this._world);
-
-    // Floor
-    const floorBody = new CANNON.Body({ mass: 0, material: DiceManager.floorBodyMaterial });
-    floorBody.addShape(new CANNON.Plane());
-    floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
-    this._world.addBody(floorBody);
-
-    // Wall 1
-    const wallShape = new CANNON.Box(new CANNON.Vec3(2, 5, 30));
-    const wallBody = new CANNON.Body({ mass: 0 });
-    wallBody.addShape(wallShape);
-    wallBody.position.set(17, 0, 0);
-    this._world.addBody(wallBody);
-
-    // Wall 2
-    const wallShape2 = new CANNON.Box(new CANNON.Vec3(30, 5, 2));
-    const wallBody2 = new CANNON.Body({ mass: 0 });
-    wallBody2.addShape(wallShape2);
-    wallBody2.position.set(0, 0, 17);
-    this._world.addBody(wallBody2);
-
-    // Dice
-    const colors = ['#ff0000', '#ffff00', '#00ff00', '#0000ff', '#ff00ff'];
-    for (let i = 0; i < 5; i++) {
-      const die = new DiceD6({ size: 1.5, backColor: colors[i] });
-      // this._scene.add(die.getObject());
-      this._dice.push(die);
-    }
-
+    container.appendChild(this._diceService.rendererDOMElement);
+    container.appendChild(this._diceService.statsDOMElement);
+    this._diceService.results.subscribe(results => this.results = results);
     requestAnimationFrame(() => this.animate());
   }
 
@@ -174,129 +31,18 @@ export class RollComponent implements OnInit, OnDestroy {
   }
 
   roll() {
-    this.randomDiceThrow();
+    this._diceService.randomDiceThrow();
   }
 
   toggleStatsVisibility() {
-    this._stats.domElement.style.visibility = this._stats.domElement.style.visibility === 'hidden' ? 'visible' : 'hidden';
-    this.statsShowing = this._stats.domElement.style.visibility === 'visible';
-  }
-
-  private randomDiceThrow() {
-    if (this.diceAddedToScene === false) {
-      this._dice.forEach(d => this._scene.add(d.getObject()));
-      this.diceAddedToScene = true;
-    }
-
-    const diceValues = [];
-
-    for (let i = 0; i < this._dice.length; i++) {
-      const yRand = Math.random() * 20;
-      this._dice[i].getObject().position.x = -15 - (i % 3) * 1.5;
-      this._dice[i].getObject().position.y = 2 + Math.floor(i / 3) * 1.5;
-      this._dice[i].getObject().position.z = -15 + (i % 3) * 1.5;
-      this._dice[i].getObject().quaternion.x = (Math.random() * 90 - 45) * Math.PI / 180;
-      this._dice[i].getObject().quaternion.z = (Math.random() * 90 - 45) * Math.PI / 180;
-      this._dice[i].updateBodyFromMesh();
-      const rand = Math.random() * 5;
-      this._dice[i].getObject().body.velocity.set(25 + rand, 40 + yRand, 15 + rand);
-      this._dice[i].getObject().body.angularVelocity.set(20 * Math.random() - 10, 20 * Math.random() - 10, 20 * Math.random() - 10);
-      diceValues.push({ dice: this._dice[i], value: i + 1 });
-    }
-    this.prepareValues(diceValues);
-  }
-
-  /**
-   *
-   * @param {array} diceValues
-   * @param {DiceObject} [diceValues.dice]
-   * @param {number} [diceValues.value]
-   *
-   */
-  private prepareValues(diceValues) {
-    if (this._throwRunning) {
-      throw new Error('Cannot start another throw. Please wait, till the current throw is finished.');
-    }
-
-    for (let i = 0; i < diceValues.length; i++) {
-      if (diceValues[i].value < 1 || diceValues[i].dice.values < diceValues[i].value) {
-        throw new Error('Cannot throw die to value ' +
-          diceValues[i].value +
-          ', because it has only ' +
-          diceValues[i].dice.values +
-          ' sides.');
-      }
-    }
-
-    this._throwRunning = true;
-
-    for (let i = 0; i < diceValues.length; i++) {
-      diceValues[i].dice.simulationRunning = true;
-      diceValues[i].vectors = diceValues[i].dice.getCurrentVectors();
-      diceValues[i].stableCount = 0;
-    }
-
-    const check = () => {
-      let allStable = true;
-      for (let i = 0; i < diceValues.length; i++) {
-        if (diceValues[i].dice.isFinished()) {
-          diceValues[i].stableCount++;
-        } else {
-          diceValues[i].stableCount = 0;
-        }
-
-        if (diceValues[i].stableCount < 50) {
-          allStable = false;
-        }
-      }
-
-      if (allStable) {
-        this.results = this._dice.map(d => {
-          return {
-            value: d.getUpsideValue(),
-            color: (<any>d).diceColor
-          };
-        });
-        console.log('every dice is finished');
-        DiceManager.world.removeEventListener('postStep', check);
-        for (let i = 0; i < diceValues.length; i++) {
-          diceValues[i].dice.setVectors(diceValues[i].vectors);
-          diceValues[i].dice.simulationRunning = false;
-        }
-
-        this._throwRunning = false;
-      } else {
-        DiceManager.world.step(DiceManager.world.dt);
-      }
-    };
-
-    this._world.addEventListener('postStep', check);
+    this.statsShowing = this._diceService.toggleStatsVisibility();
   }
 
   private animate() {
-    this.updatePhysics();
-    this.render();
-    this.update();
+    this._diceService.updatePhysics();
+    this._diceService.render();
+    this._diceService.update();
     requestAnimationFrame(() => this.animate());
-  }
-
-  private updatePhysics() {
-    this._world.step(1.0 / 60.0);
-    // tslint:disable-next-line:prefer-const
-    for (let i in this._dice) {
-      if (this._dice.hasOwnProperty(i)) {
-        this._dice[i].updateMeshFromBody();
-      }
-    }
-  }
-
-  private update() {
-    this._controls.update();
-    this._stats.update();
-  }
-
-  private render() {
-    this._renderer.render(this._scene, this._camera);
   }
 
 }
